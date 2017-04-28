562ddd990584dc725897121c30f5d117c47a17be
Daniel Kończyk <daniel@konczyk.net>
2017-04-28 23:39:46 +0200

    Fix typo

--- src/lesson6/NumberOfDiscIntersections.scala
+++ src/lesson6/NumberOfDiscIntersections.scala
@@ -5,15 +5,15 @@ object NumberOfDiscIntersections {
     val heads = a.zipWithIndex.map{case(r,i) => i.toLong - r}.sorted
     val tails = a.zipWithIndex.map{case(r,i) => i.toLong + r}.sorted
 
-    def swipe(i: Int, bound: Long, acc: Int): Int =
+    def sweep(i: Int, bound: Long, acc: Int): Int =
       if (i == heads.length || heads(i) > bound) acc
-      else swipe(i+1, bound, acc+1)
+      else sweep(i+1, bound, acc+1)
 
     def go(t: Int, h: Int, prev: Int, acc: Long): Int = {
       if (acc > 10000000) -1
       else if (t == tails.length) acc.toInt
       else {
-        val discs = swipe(h, tails(t), 0)
+        val discs = sweep(h, tails(t), 0)
         val change = prev + discs - 1
         go(t+1, h+discs, change, acc+change)
       }
